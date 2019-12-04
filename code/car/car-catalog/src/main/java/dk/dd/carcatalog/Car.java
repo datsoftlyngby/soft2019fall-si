1104e8a2ca9c40a44fdf78d509115e4a35fad9fe
Dora Di <tdi@cphbusiness.dk>
2019-12-04 12:02:43 +0100

    first push

---
+++ code/car/car-catalog/src/main/java/dk/dd/carcatalog/Car.java
@@ -0,0 +1,25 @@
+package dk.dd.carcatalog;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Car
+{
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+        @NonNull private String brand;
+        @NonNull private int year;
+        @NonNull private long km;
+}
---
+++ code/car/car-catalog/src/main/java/dk/dd/carcatalog/CarCatalogApplication.java
@@ -0,0 +1,13 @@
+package dk.dd.carcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCatalogApplication.class, args);
+    }
+
+}
